e3eccac9252c638922b5d545a20857c39a64b77f
janelle-lr <35312702+janelle-lr@users.noreply.github.com>
2021-06-08 16:55:27 +1200

    Add files via upload

--- schoolManagementSystem/schoolManagementSystem.cpp
+++ schoolManagementSystem/schoolManagementSystem.cpp
@@ -2,7 +2,9 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -12,12 +14,21 @@ struct Account {
 };
 
 struct Student {
-	string studentName;
+	int id;
+	string firstName;
+	string lastName;
 	int classNum;
 	int yearGroup;
 	string parentName;
 	string parentEmail;
 	string parentGender;
+	Account accountDetails;
+};
+
+struct Admin {
+	string firstName;
+	string lastName;
+	string email;
 	Account accountDetails;
 };
 
@@ -62,15 +73,121 @@ void introScreen(){
 
 }
 
-//Janelle's code
+//Janelle's code start
 void signIn() {
 	cout << "Enter Username : ";
 	//cin >> userName;
+}
+//Janelle's code end
+
+//Janelle's code start
+void adminSignUp(struct Admin* a) {
+	ofstream adminFile;//open admin file
+	adminFile.open("admins.csv", ios::app);
+
+	cout << "Admin Sign Up" << endl;
+	cout << "***********************" << endl;
+	cout << "Enter First Name : ";
+	cin >> a->firstName;
+	cout << "Enter Last Name : ";
+	cin >> a->lastName;
+	cout << "Enter email : ";
+	cin.ignore();
+	getline(cin, a->email);
+	cout << "Enter Username : ";
+	cin >> a->accountDetails.username;
+	cout << "Enter Passsword : ";
+	cin >> a->accountDetails.password;
+
+	adminFile << a->firstName << "," << a->lastName << "," << a->email << "," << a->accountDetails.username  << "," << a->accountDetails.password; //writing to file
+
+	adminFile.close();//close admin file
+}
+//Janelle's code end
+
+//Janelle's code start
+void adminSignIn(struct Admin* a) {
+	ifstream adminFile;//open admin file
+	string username, password, line;
+	string fn, ln, em, un, pw;
+	adminFile.open("admins.csv", ios::in);
+
+	cin.ignore();
+	while (getline(adminFile, line)) {
+		//cout << line;
+		istringstream ss(line);
+		//getline(linestream, fn,',');
+		ss >> fn >> ln >> em >> un >> pw;
+		cout << ln;
+		//if (username == ln) {
+		//	cout << "Enter Passsword : ";
+		//}
+		//else {
+		//	cout << "no";
+		//}
+	}
+	/*cout << "Enter Passsword : ";*/
+	/*cin >> password;*/
+
+	//adminFile << a->firstName << "," << a->lastName << "," << a->email << "," << a->accountDetails.username << "," << a->accountDetails.password; //writing to file
+
+	cout << "Admin Sign In" << endl;
+	cout << "***********************" << endl;
+	cout << "Enter Username : ";
+	cin >> username;
+
+	adminFile.close();//close admin file
+}
+//Janelle's code end
+
+//Janelle's code start
+void signUpMenu(struct Admin* a) {
+	int menuOption = 0;
+
+	schoolTitle();
+
+	while (menuOption != 4) {
+		cout << "\nWhat is your position type?";
+		cout << "\n1. Teacher" << endl;
+		cout << "2. Admin" << endl;
+		cout << "3. Parent" << endl;
+		cout << "4. Student" << endl;
+		cout << "\nEnter number for chosen option : ";
+		cin >> menuOption;
+		cout << "\n";
+
+		switch (menuOption) {
+		case 1:
+			adminSignIn(a);
+			break;
+		case 2:
+			adminSignUp(a);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+
+		}
+	}
+}
+//Janelle's code end
+
+void TeacherSignUp() {
+	schoolTitle();
+	/*cout << "Enter First Name : ";
+	cin >>  
+	cout << "Enter Username : ";*/
+
 }
 
 int main()
 {
 	//Janelle's Code start
+	struct Student stdnt[6];
+	struct Student* s = stdnt;
+	struct Admin admn;
+	struct Admin* adminPointer = &admn;
 	int menuOption = 0;
 
 	schoolTitle();//calling the school name
@@ -85,10 +202,11 @@ int main()
 		
 		switch (menuOption) {
 		case 1:
-			signIn();
+			//signIn();
+			adminSignIn(adminPointer);
 			break;
 		case 2:
-			//sign up;
+			signUpMenu(adminPointer);
 			break;
 		case 3:
 			//terminate program
